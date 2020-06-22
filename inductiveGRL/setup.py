152e6fbd49bbd4c6286b72e7535d6bc2fb978acc
Rafael Van Belle <rafke.vb@gmail.com>
2020-06-22 10:50:01 +0200

    local packaging

---
+++ inductiveGRL/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='inductiveGRL',
+      version='0.1',
+      description="the code used in the experimental setup of our paper 'Inductive Graph Representation Learning for Fraud Detection'.",
+      url='https://github.com/Charlesvandamme/Inductive-Graph-Representation-Learning-for-Fraud-Detection',
+      author='Charles Van Damme',
+      author_email='charles.vandamme@student.kuleuven.be',
+      license='MIT',
+      packages=['inductiveGRL'],
+      zip_safe=False)
