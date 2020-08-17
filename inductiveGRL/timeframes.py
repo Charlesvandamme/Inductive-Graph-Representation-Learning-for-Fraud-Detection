cce20be32f771dba5b04b0e9aacf91c338e4b14f
Rafael Van Belle <rafke.vb@gmail.com>
2020-08-17 15:06:33 +0200

    changed timeframes.py structures

--- inductiveGRL/timeframes.py
+++ inductiveGRL/timeframes.py
@@ -7,6 +7,7 @@ Created on Thu Jun 11 12:15:40 2020
 from datetime import timedelta
 import dateparser
 import pandas as pd
+from pandas.array import DatetimeArray
 
 class Timeframes:
 
@@ -17,21 +18,36 @@ class Timeframes:
     ----------
     date_column : Pandas dataframe
         A one column pandas dataframe containing the dates associated with records.
-    step_size : int
+    step_size : datetime.timedelta
         Step size (days) defines how many days are between the start days of two consecutive timeframes.
-    window_size: int
+    window_size: datetime.timedelta
         Defnes the window size (days) of a timeframe.
     """     
     
     
     
-    def __init__(self, date_column, step_size=None, window_size=None):
+    def __init__(self, date_column, step_size=None, train_size=None, test_size=None):
         
-        
-        self.date_column = pd.DataFrame(date_column)
+        if not isinstance(step_size, timedelta):
+            raise ValueError('step_size should be of type datetime.timedelta')
+
+        if not isinstance(train_size, timedelta):
+            raise ValueError('train_size should be of type datetime.timedelta')
+
+        if not isinstance(test_size, timedelta):
+            raise ValueError('test_size should be of type datetime.timedelta')
+
+        if not isinstance(date_column, DatetimeArray):
+            try:
+                date_column = DatetimeArray(datecolumn)
+            except:
+                print("The provided date column cannot be parsed. Please provide a datetime column in pandas DatetimeArray format.")
+
+        self.date_column = date_column
         self.step_size = step_size
-        self.window_size = window_size
-        
+        self.train_size = train_size
+        self.test_size = test_size
+        self.fold_size = self.train_size + self.test_size
     
     def get_number_of_days(self):
         
@@ -76,6 +92,25 @@ class Timeframes:
 
         return data.loc[train_indices], data.loc[inductive_indices]
     
+    def split(self): 
+
+        train_indices = []
+        test_indices = []
+        first_timestamp = min(self.date_column)
+        last_timestamp = max(self.date_column)
+        current_timestamp = first_timestamp
+        while current_timestamp < last_timestamp - self.fold_size:
+            start_train = current_timestamp
+            end_train = current_timestamp + self.train_size
+            start_test = end_train
+            end_test = start_test + self.test_size
+
+            train_indices.append(((self.date_column >= start_train) & (self.date_column < end_train)).nonzero()[0])
+            test_indices.append(((self.date_column >= start_test) & (self.date_column < end_test)).nonzero()[0])
+
+            current_timestamp = current_timestamp + self.step_size
+
+        return train_indices, test_indices
     
     def get_timeframe_indices(self, timeframe):
         """
